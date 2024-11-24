19fedc1e7a266e7329d2671e0dee095a813a5a70
Natalia Sereda <seredanatasha@gmail.com>
2024-11-24 15:55:03 +0400

    move index file to the root folder

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.scss';
-import App from './App';
+import './src/index.scss';
+import App from './src/App';
 import "macro-css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
